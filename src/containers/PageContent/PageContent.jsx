16abd6eb58d072072b42b506d1bbdbd2fc38f3ac
Mike Okoth <65109328+myk4040okothogodo@users.noreply.github.com>
2022-04-11 21:54:05 +0300

    Update PageContent.jsx

--- src/containers/PageContent/PageContent.jsx
+++ src/containers/PageContent/PageContent.jsx
@@ -2,7 +2,8 @@ import React,{useState} from 'react';
 import { Sentence } from '../../components';
 import './PageContent.css';
 
-
+/* in thie component i destructrure the content and tokes from the page props passed from the parent component <FullpageView />*/
+/*I then invoke the subcomponent <Sentence > passing the destrcutrurd props to it*/
 const PageContent = ({content , tokens}) => {
         
         const [clicked, setClicked] = useState(false);
