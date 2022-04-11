9614d0b175e0369c7fd2bb462dc3f0e8a9b3f9fb
Mike Okoth <65109328+myk4040okothogodo@users.noreply.github.com>
2022-04-11 21:35:22 +0300

    comments

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,16 @@
+
+/*in this file is where i construct my query to the provided API as an asynchronous function, that returns a promise,
+as can be seen below i use the library 'graphql-request', this code has been placed in this folder to allow for future expansion
+of the query, and also to create a single file that handles query cnstruction
+*/
+
 import { request, gql } from 'graphql-request';
 const graphqlAPI = "https://fullstack-engineer-test-n4ouilzfna-uc.a.run.app/graphql";
 
 
 
-export const getBookPages = async () => {
-    const query = gql `
+export const getBookPages = async () => {   /*the following is the recommended format for writing a graphql query, not the only way */
+    const query = gql `                 
         query MyQuery {
             book {
               pages {
@@ -18,5 +24,5 @@ export const getBookPages = async () => {
         }
       `;
     const results = await  request(graphqlAPI, query);
-    return results.book.pages;
+    return results.book.pages;      /* my return value that i will then look to plug into my application state*/
 };
