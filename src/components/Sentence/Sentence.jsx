2e9816c2da0132f5db6e420ee18e9982eab61ae0
Mike Okoth <65109328+myk4040okothogodo@users.noreply.github.com>
2022-04-11 21:59:51 +0300

    Update Sentence.jsx

--- src/components/Sentence/Sentence.jsx
+++ src/components/Sentence/Sentence.jsx
@@ -2,14 +2,14 @@ import React, {useState } from 'react';
 import Word from '../Word/Word';
 import './Sentence.css';
 
-
+/* In this component i set two state variables, isClicked and Clicked token, isClicked will be used to implemnent conditional rendering below*/
 const Sentence = ({ content, tokens ,clicked}) => {
     const [isClicked, setIsClicked] = useState(false);
     const [clickedToken, setClickedToken] = useState(null);
 
 
-    const handleClick = (word) => {
-        console.log(word, " clicked");
+    const handleClick = (word) => {       /* this handler takes the clicked word and matches it to a token, using this token to set the cllickedToken state*/
+        console.log(word, " clicked");    /* and also setting the isClicked to true */
         tokens.map((token) => {
             if (token.value === word){
                 setIsClicked(true);
@@ -21,7 +21,8 @@ const Sentence = ({ content, tokens ,clicked}) => {
     };
 
     const renderParagraph = () => content.split(" ").map((word) => <span className="book__word "  onClick={() => handleClick(word) }>{" "}{word}{" "}</span> );
-
+    /* the function simply goes throught the content word by word re-labelling each word in a span tag and assingine an onClick listener to each word*/
+    /* balow we implement the codnitional rendering of either the normal page view, or a word-token card if  a word has been clicked.*/
     return (
         <div>
       {!isClicked ? (
